2434301c387e7d5b449ad0ba3a5e10d3426badca
Neel-Darji77 <neeldarji7777@gmail.com>
2026-06-15 17:32:30 +0530

    Role based Authentication is added.

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,12 +1,12 @@
 import api from "../utils/api";
 
 export const loginUser = async (credentials) => {
-    return await api("/api/auth/login", {
+    return await api("api/auth/login", {
         method: "POST",
         body: JSON.stringify(credentials)
     });
 };
 
 export const getCurrentUser = async () => {
-    return await api("/api/auth/me");
+    return await api("api/auth/me");
 };
