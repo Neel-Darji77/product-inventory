f5f587d0006cf5d37cd7b3e6ab9355f99c390587
Neel-Darji77 <neeldarji7777@gmail.com>
2026-06-17 11:35:08 +0530

    Changing image size in product card

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -10,7 +10,7 @@ function ProductList({
     return (
         <div className="p-5">
             <AnimatePresence>
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
                     {
                         products.map((product) => (
                             <motion.div
